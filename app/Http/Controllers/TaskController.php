db14af551ddef7a47b1136c7f543ab01a71af30a
yura <yura@com.ua>
2017-09-25 21:09:17 +0300

    all

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Repositories\TaskRepository;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class TaskController extends Controller {
+      protected $tasks;
+
+    /**
+     * Создание нового экземпляра контроллера.
+     *
+     * @return void
+     */
+    public function __construct(TaskRepository $tasks) {
+        $this->middleware('auth');
+        $this->tasks=$tasks;
+    }
+
+    /**
+     * Отображение списка всех задач пользователя.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+
+    /**
+     * Показать список всех задач пользователя.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request) {
+        $tasks = $request->user()->tasks()->get();
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+        return redirect('/tasks');
+    }
+
+    /**
+     * Создание новой задачи.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
+
+}
